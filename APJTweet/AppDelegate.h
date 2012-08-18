40c2a362cfa3ff9c23aa1adbe95d353d28938571
Norimasa Nabeta <nabeta@mars.dti.ne.jp>
2012-08-19 08:03:37 +0900

    Initial Commit

---
+++ APJTweet/AppDelegate.h
@@ -0,0 +1,15 @@
+//
+//  AppDelegate.h
+//  APJTweet
+//
+//  Created by Norimasa Nabeta on 2012/08/19.
+//  Copyright (c) 2012年 Norimasa Nabeta. All rights reserved.
+//
+
+#import <UIKit/UIKit.h>
+
+@interface AppDelegate : UIResponder <UIApplicationDelegate>
+
+@property (strong, nonatomic) UIWindow *window;
+
+@end
---
+++ APJTweet/ViewController.h
@@ -0,0 +1,13 @@
+//
+//  ViewController.h
+//  APJTweet
+//
+//  Created by Norimasa Nabeta on 2012/08/19.
+//  Copyright (c) 2012年 Norimasa Nabeta. All rights reserved.
+//
+
+#import <UIKit/UIKit.h>
+
+@interface ViewController : UIViewController
+
+@end
